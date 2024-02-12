f5769a24738ae2240e33daafd17c0d7a417809ae
digitalvoice-nz <105614349+digitalvoice-nz@users.noreply.github.com>
2024-02-12 21:31:48 +1300

    wp-content updates for pricing page formatting

--- wp-content/themes/astra-child/script/ga-custom-events.js
+++ wp-content/themes/astra-child/script/ga-custom-events.js
@@ -1,23 +1,18 @@
 // @ts-nocheck
 document.addEventListener( "DOMContentLoaded", function () {
     function logRecommendedPlan() {
+       let teamPlanPrice = document.getElementById('pro-plan-price');
+        let organizationPlanPrice = document.getElementById('organization-plan-price');
+        let currentRecommendedPlan = document.querySelector(".price-plan-card.recommended-plan");
+        let planName = currentRecommendedPlan ? currentRecommendedPlan.querySelector(".plan-name").innerText : 'Self Plan';
+        let planPrice = planName === 'Enterprise Plan' ? organizationPlanPrice.innerText : planName === 'Pro Plan' ? teamPlanPrice.innerText : '0';
 
-        // Check if the button has the 'enabled-btn' class
-        if (document.getElementById("view-plans").classList.contains("enabled-btn")) {
-
-            let teamPlanPrice = document.getElementById('pro-plan-price');
-            let organizationPlanPrice = document.getElementById('organization-plan-price');
-            let currentRecommendedPlan = document.querySelector(".price-plan-card.recommended-plan");
-            let planName = currentRecommendedPlan ? currentRecommendedPlan.querySelector(".plan-name").innerText : 'Self Plan';
-            let planPrice = planName === 'Enterprise Plan' ? organizationPlanPrice.innerText : planName === 'Pro Plan' ? teamPlanPrice.innerText : '0';
-
-            gtag("event", "Recommended Plan", {
-                event_category: "Recommended Plan",
-                action: "click",
-                event_label: planName,
-                event_plan_price: planPrice,
-            });
-        }
+        gtag("event", "Recommended Plan", {
+            event_category: "Recommended Plan",
+            action: "click",
+            event_label: planName,
+            event_plan_price: planPrice,
+        });
     }
 
     // Google Analytics Event Tracking For user type selection
@@ -54,33 +49,47 @@ document.addEventListener( "DOMContentLoaded", function () {
         } );
     } );
 
-    // Google Analytics Event Tracking For View Plans Button Click 
-    document.getElementById("view-plans").addEventListener("click", function () {
-        gtag("event", "View Plans Button Click", {
-            event_category: "View Plans",
-            action: "click",
-            event_label: this.innerText,
-            event_status: this.classList.contains('enabled-btn') ? 'success' : 'failed',
-        });
-        logRecommendedPlan();
-    });
+   // Google Analytics Event for Buttons with class pricing-cta-btn cta-btn and use the event label as the button's data attribute called data-pricing-plan
+    document.querySelectorAll(".pricing-cta-btn").forEach(function (button) {
+        button.addEventListener("click", function () {
 
-    // Google Analytics Event for Buttons with class pricing-cta-btn cta-btn and use the event label as the button's data attribute called data-pricing-plan
-    document.querySelectorAll( ".pricing-cta-btn" ).forEach( function ( button ) {
-        button.addEventListener( "click", function () {
             let teamPlanPrice = document.getElementById('pro-plan-price');
             let organizationPlanPrice = document.getElementById('organization-plan-price');
-            let planName = this.getAttribute( "data-plan-name" );
+            let planName = this.getAttribute("data-plan-name");
 
-            gtag( "event", "Sign Up Button Click", {
-                event_category: "Sign Up",
-                action: "click",
-                event_label: planName,
-                event_plan_price: planName === 'Enterprise Plan' ? organizationPlanPrice.innerText : planName === 'Pro Plan' ? teamPlanPrice.innerText : '0',
+            // Check if the required inputs are filled
+            const requiredTextInputs = Array.from(document.querySelectorAll('input[required]'));
+            const emailInput = document.getElementById('email');
+            const emailIsValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value);
+            const checkboxes = document.querySelectorAll('input[name="spec"]');
+            const checkboxChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
 
-            } );
-        } );
-    } );
+            if (emailIsValid && checkboxChecked && requiredTextInputs.every(input => input.value.trim() !== '')) {
+                // If all required inputs are filled, fire the sign up event
+                gtag("event", "Sign Up Button Click", {
+                    event_category: "Sign Up",
+                    action: "click",
+                    event_label: planName,
+                    event_status: 'success',
+                    event_plan_price: planName === 'Enterprise Plan' ? organizationPlanPrice.innerText : planName === 'Pro Plan' ? teamPlanPrice.innerText : '0',
+                });
+            } else {
+                // If not all required inputs are filled, fire a different event
+                // Collect the names of the unfilled inputs
+                const unfilledInputs = requiredTextInputs.filter(input => input.value.trim() === '').map(input => input.id);
+                if (!emailIsValid) unfilledInputs.push(emailInput.id);
+                if (!checkboxChecked) unfilledInputs.push('api-spec-checkboxes');
+
+                gtag("event", "Sign Up Attempt Failed", {
+                    event_category: "Sign Up",
+                    action: "click",
+                    event_label: "Required inputs not filled",
+                    event_unfilled_inputs:  Array.from(new Set(unfilledInputs || [])).join(', '),
+                    event_status: 'failed',
+                });
+            }
+        });
+    });
 
     // Google Analytics Event For Feature Links Clicks
     document.getElementById( "pricing-plans-container" ).querySelectorAll( "a" ).forEach( function ( link ) {
--- wp-content/themes/astra-child/script/pricing.js
+++ wp-content/themes/astra-child/script/pricing.js
@@ -2,227 +2,227 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-// Selectors
-const planCards = document.querySelectorAll(".specmatic-user-type-card");
-const pricingForm = document.getElementById("pricing-form");
-const inputContainers = document.querySelectorAll(".input-container");
-const pricingPlansContainer = document.querySelector("#pricing-plans-container");
-const viewPlansButton = document.getElementById("view-plans");
-const sliders = document.querySelectorAll(".spec-slider");
-const emailInput = document.getElementById('email');
-const checkboxes = document.querySelectorAll('input[name="spec"]');
-const allInputs = document.querySelectorAll('input');
-const organizationPlanPrice = document.getElementById('organization-plan-price');
-const teamPlanPrice = document.getElementById('pro-plan-price');
-const requiredTextInputs = document.querySelectorAll('input[required]');
+    // Selectors
+    const planCards = document.querySelectorAll(".specmatic-user-type-card");
+    const pricingForm = document.getElementById("pricing-form");
+    const inputContainers = document.querySelectorAll(".input-container");
+    const pricingPlansContainer = document.querySelector("#pricing-plans-container");
+    const sliders = document.querySelectorAll(".spec-slider");
+    const checkboxes = document.querySelectorAll('input[name="spec"]');
+    const allInputs = document.querySelectorAll('input');
+    const pricingSignUpButtons = document.querySelectorAll('.price-plan-card .pricing-cta-btn');
+    const selectButtons = document.querySelectorAll('.select-btn');
 
-// Variables
-let emailFilled = emailInput.value !== '';
-let checkboxChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
-
-// Check if the button should be enabled
-function checkConditions() {
-    
-    if (emailInput.value !== '' && Array.from(checkboxes).some(checkbox => checkbox.checked) &&  Array.from(requiredTextInputs).every(input => input.value.trim() !== '')){
-        viewPlansButton.classList.remove('disabled-btn');
-        viewPlansButton.classList.add('enabled-btn');
-        emailFilled = true;
-        checkboxChecked = true;
-    } else {
-        viewPlansButton.classList.add('disabled-btn');
-        viewPlansButton.classList.remove('enabled-btn');
-        emailFilled = false;
-        checkboxChecked = false;
+    function toggleActivePlan(card) {
+        planCards.forEach((card) => card.classList.remove("active-plan"));
+        card.classList.add("active-plan");
+        calculatePrice();
     }
-}
 
-// Listen for input events on the email field
-emailInput.addEventListener('input', checkConditions);
-
-// Listen for change events on the checkboxes
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', checkConditions);
-});
-
-// Listen for input events on the text inputs
-requiredTextInputs.forEach(input => input.addEventListener('input', checkConditions));
-
-function toggleActivePlan(card) {
-    planCards.forEach((card) => card.classList.remove("active-plan"));
-    card.classList.add("active-plan");
-}
-
-function toggleRecommendedPlan(planType) {
-    const pricePlanCards = document.querySelectorAll(".price-plan-card");
-    pricePlanCards.forEach((card) =>
-        card.classList.remove("recommended-plan")
-    );
-
-    const selectedPlan = document.getElementById(planType + "-plan");
-    selectedPlan.classList.add("recommended-plan");
-
-    const recommendedHeader = selectedPlan.querySelector(".recommended-header");
-    recommendedHeader.style.display = "block";
-
-    const otherPlanCards = document.querySelectorAll(
-        ".price-plan-card:not(#" + planType + "-plan)"
-    );
-    otherPlanCards.forEach((card) => {
-        const otherRecommendedHeader = card.querySelector(".recommended-header");
-        otherRecommendedHeader.style.display = "none";
-    });
-}
-
-function handleSlider(slider) {
-    const output = slider.parentNode.querySelector(".slider-value");
-
-    slider.oninput = function () {
-        output.innerHTML = this.value;
-
-        const percent = ((this.value - this.min) / (this.max - this.min)) * 100;
-        output.style.left = `calc(${percent}% - (${output.offsetWidth / 2}px))`;
-        slider.style.background = `linear-gradient(to right, #580089 0%, #580089 ${percent}%, #ccc ${percent}%, #ccc 100%)`;
-
-        output.classList[this.value > 1 ? "add" : "remove"]("show");
-    };
-}
-
-// Event Listeners
-planCards.forEach((planCard) => {
-    planCard.addEventListener("click", function () {
-        toggleActivePlan(this);
-
-        pricingForm.classList.add("active-form");
-
-        const selectedPlanType = this.getAttribute("data-plan");
-        document.getElementById("selected-plan-type").value = selectedPlanType;
-
-        inputContainers.forEach(
-            (container) => (container.style.display = "none")
+    function toggleRecommendedPlan(planType) {
+        const pricePlanCards = document.querySelectorAll(".price-plan-card");
+        pricePlanCards.forEach((card) =>
+            card.classList.remove("recommended-plan")
         );
-        inputContainers.forEach((container) => {
-            if (container.getAttribute("data-plan").includes(selectedPlanType)) {
-                container.style.display = "block";
+
+        const selectedPlan = document.getElementById(planType + "-plan");
+        selectedPlan.classList.add("recommended-plan");
+
+        const recommendedHeader = selectedPlan.querySelector(".recommended-header");
+        recommendedHeader.style.display = "block";
+
+        const otherPlanCards = document.querySelectorAll(
+            ".price-plan-card:not(#" + planType + "-plan)"
+        );
+        otherPlanCards.forEach((card) => {
+            const otherRecommendedHeader = card.querySelector(".recommended-header");
+            otherRecommendedHeader.style.display = "none";
+        });
+    }
+
+    function handleSlider(slider) {
+        const output = slider.parentNode.querySelector(".slider-value");
+
+        // Set the initial value of the tooltip to the default value of the slider
+        output.innerHTML = slider.value;
+
+        // Calculate the initial position of the tooltip
+        const initialPercent = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+        output.style.left = `calc(${initialPercent}% - (${output.offsetWidth / 2}px))`;
+        slider.style.background = `linear-gradient(to right, #580089 0%, #580089 ${initialPercent}%, #ccc ${initialPercent}%, #ccc 100%)`;
+
+        // Show the tooltip from the start
+        output.classList.add("show");
+
+        slider.oninput = function () {
+            output.innerHTML = this.value;
+            const percent = ((this.value - this.min) / (this.max - this.min)) * 100;
+            output.style.left = `calc(${percent}% - (${output.offsetWidth / 2}px))`;
+            slider.style.background = `linear-gradient(to right, #580089 0%, #580089 ${percent}%, #ccc ${percent}%, #ccc 100%)`;
+        };
+    }
+
+
+    // Event Listeners
+    planCards.forEach((planCard) => {
+        planCard.addEventListener("click", function () {
+            toggleActivePlan(this);
+
+            pricingForm.classList.add("active-form");
+
+            const selectedPlanType = this.getAttribute("data-plan");
+            document.getElementById("selected-plan-type").value = selectedPlanType;
+
+            // If the data-plan attribute of the clicked card is not "self" then disable button with data-plan-name "Basic Plan" and enable the rest
+            if (selectedPlanType !== "self") {
+                selectButtons.forEach((button, index) => {
+                    button.disabled = index === 0;
+                }
+                );
+            } else {
+                selectButtons.forEach((button) => {
+                    button.disabled = false;
+                }
+                );
+            }
+
+
+            inputContainers.forEach(
+                (container) => (container.style.display = "none")
+            );
+            inputContainers.forEach((container) => {
+                if (container.getAttribute("data-plan").includes(selectedPlanType)) {
+                    container.style.display = "block";
+                }
+            });
+
+            if (selectedPlanType && pricingPlansContainer.classList.contains("pricing-plans-visible")
+            ) {
+                toggleRecommendedPlan(selectedPlanType);
+            }
+
+            const selfPlanClasses = document.getElementById("self-plan").classList;
+            const selfPlanSignupClasses = document.getElementById("self-plan-signup").classList;
+            if (selectedPlanType === "self") {
+                document.getElementById("async-api-spec").disabled = true;
+                selfPlanClasses.remove("plan-unavailable");
+                selfPlanSignupClasses.remove("plan-unavailable-btn");
+            } else {
+                checkboxes.forEach((checkbox) => {
+                    checkbox.disabled = false;
+                }
+                );
+                selfPlanClasses.add("plan-unavailable");
+                selfPlanSignupClasses.add("plan-unavailable-btn");
+            }
+            document.getElementById("tell-us-more").style.display = "block";
+            pricingPlansContainer.classList.remove("pricing-plans-hidden");
+            pricingPlansContainer.classList.add("pricing-plans-visible");
+            toggleRecommendedPlan(selectedPlanType);
+            // pricingPlansContainer.scrollIntoView({ behavior: 'smooth' });
+            document.getElementById('spec-checkbox-grp').scrollIntoView({ behavior: 'smooth' });
+
+        });
+    });
+
+    function intValue(el) {
+        let val = parseInt(el.value || 1);
+        if(val === 0) return 1
+        return val
+    }
+
+    function calculatePrice() {
+        let teamSize = intValue(document.getElementById('team-size'));
+        let teamCount = intValue(document.getElementById('team-count'));
+        let apiCount = intValue(document.getElementById('api-count'));
+        let buildCount = intValue(document.getElementById('build-count'));
+        const asyncAPISelected = document.getElementById("async-api-spec").checked;
+        const soapAPISelected = document.getElementById("wdsl-spec").checked;
+        let apiPriceFactor;
+
+        if (asyncAPISelected && soapAPISelected) {
+            apiPriceFactor = 2;
+        } else if (asyncAPISelected || soapAPISelected) {
+            apiPriceFactor = 1.5;
+        } else {
+            apiPriceFactor = 1;
+        }
+
+        let teamPlanPrice = document.getElementById('pro-plan-price');
+        let organizationPlanPrice = document.getElementById('organization-plan-price');     
+        let additionalPrice = parseInt((teamSize * apiCount * buildCount * apiPriceFactor) * 0.005);
+
+        // Update team plan price
+        teamPlanPrice.innerText = formatPrice(5000 + additionalPrice);
+
+        // Update organization plan price
+        organizationPlanPrice.innerText = formatPrice(25000 + (additionalPrice * teamCount));
+    }
+
+    function formatPrice(price) {
+        return price.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 });
+    }
+
+    // Add event listeners to all inputs so that the price is updated when the user changes the input
+    allInputs.forEach(input => {
+        input.addEventListener('input', calculatePrice);
+    });
+
+    sliders.forEach(handleSlider);
+
+    // Making sure that the "None" checkbox disables the other checkboxes and vice versa for API Spec checkboxes
+    const noneCheckbox = document.getElementById('no-spec');
+    const otherCheckboxes = document.querySelectorAll('input[name="spec"]:not(#no-spec)');
+
+    noneCheckbox.addEventListener('change', function() {
+        otherCheckboxes.forEach(checkbox => {
+            checkbox.disabled = noneCheckbox.checked;
+            if (noneCheckbox.checked) {
+                checkbox.checked = false;
             }
         });
-
-        if (selectedPlanType && pricingPlansContainer.classList.contains("pricing-plans-visible")
-        ) {
-            toggleRecommendedPlan(selectedPlanType);
-        }
-
-        const selfPlanClasses = document.getElementById("self-plan").classList;
-        const selfPlanSignupClasses = document.getElementById("self-plan-signup").classList;
-        if (selectedPlanType === "self") {
-            document.getElementById("async-api-spec").disabled = true;
-            selfPlanClasses.remove("plan-unavailable");
-            selfPlanSignupClasses.remove("plan-unavailable-btn");
-        } else {
-            checkboxes.forEach((checkbox) => {
-                checkbox.disabled = false;
-            }
-            );
-            selfPlanClasses.add("plan-unavailable");
-            selfPlanSignupClasses.add("plan-unavailable-btn");
-        }
-        document.getElementById("tell-us-more").style.display = "block";
     });
-});
 
-viewPlansButton.addEventListener("click", function () {
-    const unfilledTextInputs = Array.from(requiredTextInputs)
-        .filter(input => input.value.trim() === '')
-        .map(input => document.querySelector(`label[for="${input.id}"]`).innerText.replace('*', ''));
-
-    const emailInput = document.querySelector('input[name="email"]');
-    const emailIsValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value);
-    const unfilledEmail = emailFilled ? [] : [document.querySelector('label[for="email"]').innerText.replace('*', '')];
-
-    const uncheckedCheckboxes = Array.from(document.querySelectorAll('input[name="spec"]'))
-        .some(checkbox => checkbox.checked) ? [] : ["API specifications"];
-
-    const messages = [...unfilledTextInputs, ...unfilledEmail, ...uncheckedCheckboxes];
-
-    if (!emailIsValid && emailFilled) {
-        alert("The email address is not valid.");
-        return;
-    }
-
-    if (messages.length > 0) {
-        viewPlansButton.classList.add("disabled-btn");
-        alert("Please fill in these fields: " + messages.join(", ") + ".");
-        return;
-    }
-
-    viewPlansButton.classList.remove("disabled-btn");
-    viewPlansButton.classList.add("enabled-btn");
-    pricingPlansContainer.classList.remove("pricing-plans-hidden");
-    pricingPlansContainer.classList.add("pricing-plans-visible");
-    const selectedPlanType = document.getElementById("selected-plan-type").value;
-    toggleRecommendedPlan(selectedPlanType);
-    pricingPlansContainer.scrollIntoView();
-});
-
-function intValue(el) {
-    let val = parseInt(el.value || 1);
-    if(val === 0) return 1
-    return val
-}
-
-function calculatePrice() {
-    let teamSize = intValue(document.getElementById('team-size'));
-    let teamCount = intValue(document.getElementById('team-count'));
-    let apiCount = intValue(document.getElementById('api-count'));
-    let buildCount = intValue(document.getElementById('build-count'));
-
-    const asyncAPIFactor = document.getElementById("async-api-spec").checked ? 2 : 1;
-    
-    let teamPlanPrice = document.getElementById('pro-plan-price');
-    let organizationPlanPrice = document.getElementById('organization-plan-price');
-    
-    let additionalPrice = parseInt((teamSize * apiCount * buildCount * asyncAPIFactor) * 0.005);
-
-    // Update team plan price
-    teamPlanPrice.innerText = formatPrice(5000 + additionalPrice);
-
-    // Update organization plan price
-    organizationPlanPrice.innerText = formatPrice(25000 + (additionalPrice * teamCount));
-}
-
-function formatPrice(price) {
-    return price.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 });
-}
-
-// Add event listeners to all inputs so that the price is updated when the user changes the input
-allInputs.forEach(input => {
-    input.addEventListener('input', calculatePrice);
-});
-
-sliders.forEach(handleSlider);
-
-// Making sure that the "None" checkbox disables the other checkboxes and vice versa for API Spec checkboxes
-const noneCheckbox = document.getElementById('no-spec');
-const otherCheckboxes = document.querySelectorAll('input[name="spec"]:not(#no-spec)');
-
-noneCheckbox.addEventListener('change', function() {
     otherCheckboxes.forEach(checkbox => {
-        checkbox.disabled = noneCheckbox.checked;
-        if (noneCheckbox.checked) {
-            checkbox.checked = false;
-        }
+        checkbox.addEventListener('change', function() {
+            if (checkbox.checked) {
+                noneCheckbox.checked = false;
+                noneCheckbox.disabled = false;
+            }
+            else if (!Array.from(otherCheckboxes).some(checkbox => checkbox.checked)) {
+                noneCheckbox.disabled = false;
+            }
+        });
     });
-});
 
-otherCheckboxes.forEach(checkbox => {
-    checkbox.addEventListener('change', function() {
-        if (checkbox.checked) {
-            noneCheckbox.checked = false;
-            noneCheckbox.disabled = false;
-        }
-        else if (!Array.from(otherCheckboxes).some(checkbox => checkbox.checked)) {
-            noneCheckbox.disabled = false;
-        }
+    pricingSignUpButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const checkedOne = Array.prototype.slice.call(checkboxes).some(x => x.checked);
+            if (!checkedOne) {
+                document.getElementById('spec-checkbox-grp').scrollIntoView({ behavior: 'smooth' });
+                alert('Please select at least one specification');
+                return;
+            }
+    
+            const personalInfoInputs = document.getElementById('personal-info-inputs');
+            personalInfoInputs.classList.remove('hide-inputs');
+    
+            const signUpBtn = document.getElementById('sign-up-btn');
+            const planName = button.getAttribute('data-plan-name');
+            signUpBtn.setAttribute('data-plan-name', planName);
+            personalInfoInputs.scrollIntoView({ behavior: 'smooth' });
+        });
     });
-});
 
+    selectButtons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            // Change the text of all buttons back to "Select"
+            selectButtons.forEach(btn => {
+                btn.innerText = 'Select';
+            });
+    
+            // Change the text of the clicked button to a tick mark symbol and "Selected"
+            button.innerText = '✓ Selected';
+        });
+    });
 });
