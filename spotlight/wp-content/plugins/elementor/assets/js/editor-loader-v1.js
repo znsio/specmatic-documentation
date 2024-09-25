fea4c2ac2d2440cd80aa22910c37c15dc21502f8
digitalvoice-nz <105614349+digitalvoice-nz@users.noreply.github.com>
2024-09-25 23:21:26 +1200

    html update for Testmu link fixes

---
+++ spotlight/wp-content/plugins/elementor/assets/js/editor-loader-v1.js
@@ -0,0 +1,13 @@
+/*! elementor - v3.24.0 - 23-09-2024 */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*******************************************************!*\
+  !*** ../core/editor/loader/v1/js/editor-loader-v1.js ***!
+  \*******************************************************/
+
+
+window.elementor.start();
+/******/ })()
+;
+//# sourceMappingURL=editor-loader-v1.js.map
---
+++ spotlight/wp-content/plugins/elementor/assets/js/packages/editor-app-bar/editor-app-bar.strings.js
@@ -0,0 +1,32 @@
+__( 'Exit to WordPress', 'elementor' );
+__( 'Help', 'elementor' );
+__( 'More', 'elementor' );
+__( 'Elementor Logo', 'elementor' );
+__( 'Integrations', 'elementor' );
+__( 'User Preferences', 'elementor' );
+__( 'Theme Builder', 'elementor' );
+__( 'Structure', 'elementor' );
+__( 'Site Settings', 'elementor' );
+__( 'Save Changes', 'elementor' );
+__( 'Switch Device', 'elementor' );
+// translators: %s: Breakpoint label, %d: Breakpoint size.
+__( '%s (%dpx and up)', 'elementor' );
+// translators: %s: Breakpoint label, %d: Breakpoint size.
+__( '%s (up to %dpx)', 'elementor' );
+__( 'Keyboard Shortcuts', 'elementor' );
+__( 'History', 'elementor' );
+__( 'Finder', 'elementor' );
+__( 'Elements', 'elementor' );
+__( 'Widgets', 'elementor' );
+__( 'Add Element', 'elementor' );
+__( 'View Page', 'elementor' );
+__( 'Save as Template', 'elementor' );
+__( 'Save Draft', 'elementor' );
+__( 'Copy and Share', 'elementor' );
+__( 'Save Options', 'elementor' );
+__( 'Save Options', 'elementor' );
+__( 'Publish', 'elementor' );
+__( 'Submit', 'elementor' );
+__( 'Preview Changes', 'elementor' );
+/* translators: %s: Post type label. */
+__( '%s Settings', 'elementor' );
